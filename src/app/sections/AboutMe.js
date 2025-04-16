0ad54ba1f28c113deb0d3a9a445e8a1d27cd28e9
Marcos Pedro <king.frontweb@outlook.com>
2025-04-16 12:13:35 -0300

    mobile: limit text

--- src/app/sections/AboutMe.js
+++ src/app/sections/AboutMe.js
@@ -2,23 +2,23 @@ import React from 'react'
 
 const AboutMe = () => {
   return (
-    <div className='bg-[#1E1E1E] w-screen py-16'>
+    <div className='bg-[#1E1E1E] container py-16 px-6'>
       <div className="container text-white text-center">
         <h3 className="font-bold text-5xl mb-8 position w-fit mx-auto
         underline-center      
         ">Sobre mim</h3>
         <p className='text-[rgba(255, 255, 255, 0.5)] mb-6 max-w-lg mx-auto'> Sou desenvolvedor FullStack com mais de 2 anos de experiência criando interfaces modernas e sistemas robustos. Tenho paixão por transformar ideias em soluções digitais funcionais e bem estruturadas. Estou sempre em busca de aprender algo novo, melhorar meu código e entregar valor real através de cada projeto.
         </p>
-        <div className='flex justify-center items-center divide-x-2 divide-[#222]'>
-          <div className='flex flex-col px-8'>
+        <div className='flex flex-wrap justify-center items-center gap-4 divide-y md:divide-y-0 md:divide-x-2 divide-[#222]'>
+          <div className='flex flex-col md:px-8'>
             <span className='font-[Montserrat] text-5xl font-medium'>2+</span>
             <span>Anos de experiência</span>
           </div>
-          <div className='flex flex-col px-8'>
+          <div className='flex flex-col md:px-8'>
             <span className='font-[Montserrat] text-5xl font-medium'>3+</span>
             <span>Projetos realizados</span>
           </div>
-          <div className='flex flex-col px-8'>
+          <div className='flex flex-col md:px-8 mt-5 md:mt-0'>
             <span className='font-[Montserrat] text-5xl font-medium'>3</span>
             <span>Empresas trabalhadas</span>
           </div>
