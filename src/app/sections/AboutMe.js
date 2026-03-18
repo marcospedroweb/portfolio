108ae4e637157ea15143e28a4e8c96e2194cf123
marcospedroweb <king.frontweb@outlook.com>
2026-03-18 17:06:41 -0300

    refactor: change aboutMe text

--- src/app/sections/AboutMe.js
+++ src/app/sections/AboutMe.js
@@ -1,30 +1,50 @@
-import React from 'react'
-import MainHeading from '../Components/MainHeading'
+import React from 'react';
+import MainHeading from '../Components/MainHeading';
 
 const AboutMe = () => {
   return (
-    <div className='bg-[#1E1E1E] py-16'>
-      <div className="container px-6 text-center text-white scroll-mt-32" id="Sobre" data-aos="fade-right">
+    <div className="bg-[#1E1E1E] py-16">
+      <div
+        className="container px-6 text-center text-white scroll-mt-32"
+        id="Sobre"
+        data-aos="fade-right"
+      >
         <MainHeading text="Sobre mim" />
-        <p className='text-[rgba(255, 255, 255, 0.5)] mb-6 max-w-lg mx-auto'> Sou desenvolvedor FullStack com mais de 2 anos de experiência criando interfaces modernas e sistemas. Tenho paixão por transformar ideias em soluções digitais funcionais e bem estruturadas. Estou sempre em busca de aprender algo novo, melhorar minhas habilidades e conseguir o melhor desempenho em um projeto.
+        <p className="text-[rgba(255, 255, 255, 0.5)] mb-6 max-w-lg mx-auto">
+          {' '}
+          Sou uma pessoa criativa, com grande interesse por leitura e escrita de
+          poesias. Nas horas vagas, gosto de me aventurar na poesia, no desenho,
+          na pintura, além de praticar academia e artes marciais.
         </p>
-        <div className='flex flex-wrap justify-center items-center gap-4 divide-y md:divide-y-0 md:divide-x-2 divide-[#222]' data-aos="fade-left">
-          <div className='flex flex-col md:px-8'>
-            <span className='font-[Montserrat] text-5xl font-medium'>2+</span>
+        <p className="text-[rgba(255, 255, 255, 0.5)] mb-6 max-w-lg mx-auto">
+          {' '}
+          Há pouco mais de dois anos, atuo como desenvolvedor Front-End,
+          aplicando minha experiência e buscando inovação na criação de
+          interfaces modernas e sistemas. Tenho paixão por transformar ideias em
+          soluções digitais funcionais e bem estruturadas. Estou sempre em busca
+          de aprender algo novo, aprimorar minhas habilidades e entregar o
+          melhor desempenho em cada projeto.
+        </p>
+        <div
+          className="flex flex-wrap justify-center items-center gap-4 divide-y md:divide-y-0 md:divide-x-2 divide-[#222]"
+          data-aos="fade-left"
+        >
+          <div className="flex flex-col md:px-8">
+            <span className="font-[Montserrat] text-5xl font-medium">2+</span>
             <span>Anos de experiência</span>
           </div>
-          <div className='flex flex-col md:px-8'>
-            <span className='font-[Montserrat] text-5xl font-medium'>3+</span>
+          <div className="flex flex-col md:px-8">
+            <span className="font-[Montserrat] text-5xl font-medium">3+</span>
             <span>Projetos realizados</span>
           </div>
-          <div className='flex flex-col mt-5 md:px-8 md:mt-0'>
-            <span className='font-[Montserrat] text-5xl font-medium'>3</span>
+          <div className="flex flex-col mt-5 md:px-8 md:mt-0">
+            <span className="font-[Montserrat] text-5xl font-medium">3</span>
             <span>Empresas trabalhadas</span>
           </div>
         </div>
       </div>
-    </div >
-  )
-}
+    </div>
+  );
+};
 
-export default AboutMe
+export default AboutMe;
